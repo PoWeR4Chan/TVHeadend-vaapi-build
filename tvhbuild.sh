5991bc7e5be3fa70b93974b034af2519d389c140
PoWeR4Chan <63404242+PoWeR4Chan@users.noreply.github.com>
2021-07-22 15:35:59 +0900

    Add files via upload

---
+++ tvhbuild.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+cd /root/tvhjob
+
+git clone https://github.com/tvheadend/tvheadend.git 
+
+cd tvheadend
+
+AUTOBUILD_CONFIGURE_EXTRA="--enable-vaapi" ./Autobuild.sh -t buster-amd64
+# AUTOBUILD_CONFIGURE_EXTRA="--enable-libffmpeg_static --disable-bintray_cache --enable-dvbcsa" ./Autobuild.sh -t buster-amd64
+
+echo "Build Done!"
+
+make clean
+
+cd ..
+
+rm -rf tvheadend
+
+rm -f *dbg*.deb
+
+rm -f *.buildinfo
+
+rm -f *.changes
+
+echo "Now Install"
+
+dpkg -i *.deb
+
+rm -f /media/Data/Tmp/*.deb
+
+cp * /media/Data/Tmp
+
+mv * /media/Data/BackUp/tvhstore/
+
+echo
+echo "Old BackUp Erase! Before 12 months From $(date -R)"
+cd /media/Data/BackUp/tvhstore/
+find /media/Data/BackUp/tvhstore/ -mtime +365 -exec rm -f {} \;
+echo
+echo "Remain Data is...."
+ls -lh
+echo
+echo "All Done!!!!"
+
+ssmtp pwrchan@me.com < /home/chan/tvhbuild.txt
+# mutt -s "TVHeadend Build" me.pwrchan@gmail.com < /home/chan/tvhbuild.txt
